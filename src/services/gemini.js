54d50961013e571e468d6c43504e59169834e80f
GithubFarelAlghazali <farelgithub@gmail.com>
2025-05-18 22:34:54 +0700

    edit credential

--- src/services/gemini.js
+++ src/services/gemini.js
@@ -1,5 +1,5 @@
 import { GoogleGenAI } from '@google/genai'
-const GEMINI_API = process.env.API_KEY
+const GEMINI_API = process.env.VITE_GEMINI
 
 const ai = new GoogleGenAI({ apiKey: GEMINI_API })
 
